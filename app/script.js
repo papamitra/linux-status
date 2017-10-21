e4c1b3ea63cf1c31effe187c920543ba4202a254
papamitra <papamitra@gmail.com>
2017-10-21 22:27:41 +0900

    app-7

--- app/script.js
+++ app/script.js
@@ -1,11 +1,15 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
 var app = new Vue ({
-    el: '#app-5',
+    el: '#app-7',
     data: {
-        message: 'Hello Vue.js!'
-    },
-    methods: {
-        reverseMessage: function() {
-            this.message = this.message.split('').reverse().join('')
-        }
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
     }
 })
